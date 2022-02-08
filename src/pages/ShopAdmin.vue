d4dd742519d251003141b881c58891e37feb08a4
Shynu Mary Varghese <65387041+estrelabrilante@users.noreply.github.com>
2022-02-08 19:42:47 +0530

    product state

---
+++ src/pages/ShopAdmin.vue
@@ -0,0 +1,24 @@
+<template>
+  <section>
+    <div v-if="isLoggedIn">
+      <h2>Shop Admin</h2>
+      <p>There isn't much to do at the moment - sorry ...</p>
+    </div>
+  </section>
+</template>
+<script>
+export default {
+  inject: ['isLoggedIn', 'login', 'logout'],
+};
+</script>
+
+<style scoped>
+section {
+  text-align: center;
+  margin: 3rem auto;
+  max-width: 40rem;
+  border: 1px solid #ccc;
+  border-radius: 12px;
+  padding: 2rem;
+}
+</style>
